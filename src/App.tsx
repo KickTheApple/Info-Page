cf666704d1a41163aedabe5d17da445374a312d1
KickTheApple <dd62879@student.uni-lj.si>
2025-07-24 02:14:54 +0200

    Added worked on Projects

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,10 @@ export default function App() {
                 <div id={"aspects"}>
                     <p id={"workedOn"}>I've worked on the following Projects:</p>
                     <div id={"projects"}>
-
+                        <div id={"proj"}><a href={"https://github.com/KickTheApple/Sorting-Algorithms-in-Java"}>Sorting-Algorithms-in-Java</a><p>A project comrpised of many different sorting algorithms.</p></div>
+                    </div>
+                    <div id={"projects"}>
+                        <div id={"proj"}><a href={"https://github.com/KickTheApple/2D-Matrix-Displayer"}>2D-Matrix-Displayer</a><p>A simple html website for reading json files and drawing using them.</p></div>
                     </div>
                 </div>
 

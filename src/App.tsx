451196e21ac4418a02c4c6bc87a77fa436a82971
Domenic Dvojmoc <applecakes811@gmail.com>
2026-07-11 02:51:06 +0200

    Feat: Updated with current projects

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,21 @@ export default function App() {
                 <div id={"aspects"}>
                     <p id={"workedOn"}>I've worked on the following Projects:</p>
                     <div id={"projects"}>
+                        <div id={"proj"}><a href={"https://github.com/KickTheApple/SSHKotlic"}>SSHKotlic</a><p>A High interaction SSH Honeypot with keylogging and persistance combined with a singular data collection point for visualization</p></div>
+                    </div>
+                    <div id={"projects"}>
+                        <div id={"proj"}><a href={"https://github.com/KickTheApple/Slordle"}>Slordle</a><p>A Simple word game developed for React framework learning</p></div>
+                    </div>
+                    <div id={"projects"}>
+                        <div id={"proj"}><a href={"https://github.com/KickTheApple/SSHPotica"}>SSHPotica</a><p>A high interaction honeypot utilizing Docker containers written in C</p></div>
+                    </div>
+                    <div id={"projects"}>
+                        <div id={"proj"}><a href={"https://github.com/KickTheApple/CompilerPINS"}>CompilerPINS</a><p>A compiler written in Java for an imaginary programming language called PINS</p></div>
+                    </div>
+                    <div id={"projects"}>
+                        <div id={"proj"}><a href={"https://github.com/KickTheApple/STM-Mini_Games"}>STM32-Games</a><p>A collection of small games written for the STM32 Embedded system using C with HAL</p></div>
+                    </div>
+                    <div id={"projects"}>
                         <div id={"proj"}><a href={"https://github.com/KickTheApple/Sorting-Algorithms-in-Java"}>Sorting-Algorithms-in-Java</a><p>A project comrpised of many different sorting algorithms.</p></div>
                     </div>
                     <div id={"projects"}>
